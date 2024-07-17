09afd8e74b078eac1493bebb80f59040e76ca31a
github-action-benchmark <github@users.noreply.github.com>
2024-07-17 21:14:40 +0000

    add Benchmarks (go) benchmark result for 67f50c70f0a812bd556245a60fe1b946490b2ffd

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721250567668,
+  "lastUpdate": 1721250880671,
   "repoUrl": "https://github.com/XSAM/opentelemetry-go",
   "entries": {
     "Benchmarks": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 16,
             "unit": "allocs/op",
             "extra": "910605 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sam@samxie.me",
+            "name": "Sam Xie",
+            "username": "XSAM"
+          },
+          "committer": {
+            "email": "sam@samxie.me",
+            "name": "Sam Xie",
+            "username": "XSAM"
+          },
+          "distinct": true,
+          "id": "67f50c70f0a812bd556245a60fe1b946490b2ffd",
+          "message": "Trigger CI",
+          "timestamp": "2024-07-17T14:14:16-07:00",
+          "tree_id": "67abdb8f7b3ad675a7ce846d863aa95f0581c21b",
+          "url": "https://github.com/XSAM/opentelemetry-go/commit/67f50c70f0a812bd556245a60fe1b946490b2ffd"
+        },
+        "date": 1721250880315,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSpanWithAttributes_8/AlwaysSample",
+            "value": 1334,
+            "unit": "ns/op\t    1728 B/op\t      16 allocs/op",
+            "extra": "906643 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSpanWithAttributes_8/AlwaysSample - ns/op",
+            "value": 1334,
+            "unit": "ns/op",
+            "extra": "906643 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSpanWithAttributes_8/AlwaysSample - B/op",
+            "value": 1728,
+            "unit": "B/op",
+            "extra": "906643 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSpanWithAttributes_8/AlwaysSample - allocs/op",
+            "value": 16,
+            "unit": "allocs/op",
+            "extra": "906643 times\n4 procs"
           }
         ]
       }
