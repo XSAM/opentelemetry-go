655a18f0f4069195e4c848c082e1d90e648d9895
github-action-benchmark <github@users.noreply.github.com>
2024-07-17 21:09:27 +0000

    add Benchmarks (go) benchmark result for cc825ffb3e7d1acf64d48344e95224441d300fd5

---
+++ benchmarks/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1721250567668,
+  "repoUrl": "https://github.com/XSAM/opentelemetry-go",
+  "entries": {
+    "Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "sam@samxie.me",
+            "name": "Sam Xie",
+            "username": "XSAM"
+          },
+          "committer": {
+            "email": "sam@samxie.me",
+            "name": "Sam Xie",
+            "username": "XSAM"
+          },
+          "distinct": true,
+          "id": "cc825ffb3e7d1acf64d48344e95224441d300fd5",
+          "message": "Disable external-data-json-path",
+          "timestamp": "2024-07-16T21:41:00-07:00",
+          "tree_id": "67abdb8f7b3ad675a7ce846d863aa95f0581c21b",
+          "url": "https://github.com/XSAM/opentelemetry-go/commit/cc825ffb3e7d1acf64d48344e95224441d300fd5"
+        },
+        "date": 1721250567116,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkSpanWithAttributes_8/AlwaysSample",
+            "value": 1333,
+            "unit": "ns/op\t    1728 B/op\t      16 allocs/op",
+            "extra": "910605 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSpanWithAttributes_8/AlwaysSample - ns/op",
+            "value": 1333,
+            "unit": "ns/op",
+            "extra": "910605 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSpanWithAttributes_8/AlwaysSample - B/op",
+            "value": 1728,
+            "unit": "B/op",
+            "extra": "910605 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSpanWithAttributes_8/AlwaysSample - allocs/op",
+            "value": 16,
+            "unit": "allocs/op",
+            "extra": "910605 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
